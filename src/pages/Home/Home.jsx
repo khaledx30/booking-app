15051a682e48dc82b6fcd3b3fb275127aad21e09
khaled essam <khaledx30@gmail.com>
2023-08-07 19:46:23 +0300

    ds

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/header/Header";
-import styles from "./Home.module.css";
+import "./Home.css";
+import Featured from "./../../components/featured/Featured";
 export default function About() {
   return (
     <>
-      <div className={styles.color}>
+      <div className="test">
         <Navbar />
         <Header type={true} />
-        <div className="homeContainer"></div>
+        <div className="homeContainer">
+          <Featured />
+          <h1 className="homeTitle">Browse by property type</h1>
+        </div>
       </div>
     </>
   );
