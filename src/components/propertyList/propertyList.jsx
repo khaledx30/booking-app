40d0d5f45e61d93a51cc943e64c24043a454dcb2
Khaled Essam <khaledx30@gmail.com>
2023-08-14 16:16:04 +0300

    property list

---
+++ src/components/propertyList/propertyList.jsx
@@ -0,0 +1,11 @@
+import "./propertyList.css";
+
+export default function propertyList() {
+  return (
+    <div className="pList">
+      <div className="pListItem">
+        <img className="pListImg" src="" alt="" />
+      </div>
+    </div>
+  );
+}
