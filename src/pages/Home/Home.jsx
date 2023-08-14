95b4a3fdfe957020d82522e453e761de0057a66b
Khaled Essam <khaledx30@gmail.com>
2023-08-14 16:26:14 +0300

    fs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Header from "../../components/header/Header";
 import "./Home.css";
 import Featured from "./../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/propertyList";
 export default function About() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function About() {
         <div className="homeContainer">
           <Featured />
           <h1 className="homeTitle">Browse by property type</h1>
+          <PropertyList />
         </div>
       </div>
     </>
